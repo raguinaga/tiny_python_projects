63656e5b36697d95e50c7a7cf438df4f1de61ac9
Roberto Aguinaga <raguinaga@protonmail.com>
2022-11-05 17:24:23 -0500

    Added tests to fuzz the program

--- 02_crowsnest/test.py
+++ 02_crowsnest/test.py
@@ -4,15 +4,33 @@
 import os
 from subprocess import getstatusoutput, getoutput
 
-prg = './crowsnest.py'
+prg = "./crowsnest.py"
 consonant_words = [
-    'brigantine', 'clipper', 'dreadnought', 'frigate', 'galleon', 'haddock',
-    'junk', 'ketch', 'longboat', 'mullet', 'narwhal', 'porpoise', 'quay',
-    'regatta', 'submarine', 'tanker', 'vessel', 'whale', 'xebec', 'yatch',
-    'zebrafish'
+    "brigantine",
+    "clipper",
+    "dreadnought",
+    "frigate",
+    "galleon",
+    "haddock",
+    "junk",
+    "ketch",
+    "longboat",
+    "mullet",
+    "narwhal",
+    "porpoise",
+    "quay",
+    "regatta",
+    "submarine",
+    "tanker",
+    "vessel",
+    "whale",
+    "xebec",
+    "yatch",
+    "zebrafish",
 ]
-vowel_words = ['aviso', 'eel', 'iceberg', 'octopus', 'upbound']
-template = 'Ahoy, Captain, {} {} off the larboard bow!'
+vowel_words = ["aviso", "eel", "iceberg", "octopus", "upbound"]
+template = "Ahoy, Captain, {} {} off the larboard bow!"
+starboard_template = "Ahoy, Captain, {} {} off the starboard bow!"
 
 
 # --------------------------------------------------
@@ -26,10 +44,10 @@ def test_exists():
 def test_usage():
     """usage"""
 
-    for flag in ['-h', '--help']:
-        rv, out = getstatusoutput(f'{prg} {flag}')
+    for flag in ["-h", "--help"]:
+        rv, out = getstatusoutput(f"{prg} {flag}")
         assert rv == 0
-        assert out.lower().startswith('usage')
+        assert out.lower().startswith("usage")
 
 
 # --------------------------------------------------
@@ -37,8 +55,8 @@ def test_consonant():
     """brigantine -> a brigantine"""
 
     for word in consonant_words:
-        out = getoutput(f'{prg} {word}')
-        assert out.strip() == template.format('a', word)
+        out = getoutput(f"{prg} {word}")
+        assert out.strip() == template.format("a", word)
 
 
 # --------------------------------------------------
@@ -46,8 +64,8 @@ def test_consonant_upper():
     """brigantine -> a Brigatine"""
 
     for word in consonant_words:
-        out = getoutput(f'{prg} {word.title()}')
-        assert out.strip() == template.format('a', word.title())
+        out = getoutput(f"{prg} {word.title()}")
+        assert out.strip() == template.format("A", word.title())
 
 
 # --------------------------------------------------
@@ -55,8 +73,8 @@ def test_vowel():
     """octopus -> an octopus"""
 
     for word in vowel_words:
-        out = getoutput(f'{prg} {word}')
-        assert out.strip() == template.format('an', word)
+        out = getoutput(f"{prg} {word}")
+        assert out.strip() == template.format("an", word)
 
 
 # --------------------------------------------------
@@ -64,5 +82,24 @@ def test_vowel_upper():
     """octopus -> an Octopus"""
 
     for word in vowel_words:
-        out = getoutput(f'{prg} {word.upper()}')
-        assert out.strip() == template.format('an', word.upper())
+        out = getoutput(f"{prg} {word.upper()}")
+        assert out.strip() == template.format("An", word.upper())
+
+
+def test_matching_case():
+    """Checks if the article matches the word"""
+    for word in vowel_words:
+        out_upper = getoutput(f"{prg} {word.title()}")
+        assert out_upper.strip() == template.format("An", word.title())
+
+
+def test_starbaord():
+    """Checks if the --side switch outputs the word starboard"""
+    for word in vowel_words:
+        output = getoutput(f"{prg} {word} --side starboard")
+        assert output.strip() == starboard_template.format("an", word)
+
+def test_error_message():
+    """Checks that a helpful message is displayed when garbage is fed to the program"""
+    output = getoutput(f"{prg} 12324354")
+    assert output.strip() == "Your word must start with an alphabetical character"
